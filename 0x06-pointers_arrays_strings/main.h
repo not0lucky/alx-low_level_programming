3c2cbe561c4c718c743e890e7e973125670f7aeb
anir agram <anirpoke@gmail.com>
2022-09-22 06:51:36 -0700

    task6 commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ char *_srtncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
+char *cap_string(char *s);
 
 
 #endif
