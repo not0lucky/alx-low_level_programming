2126815fe2facd3fec208ad8fd2b4e16c8e53278
anir agram <anirpoke@gmail.com>
2022-09-22 06:39:09 -0700

    task2 commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n)
-
+char *_strncat(char *dest, char *src, int n);
+char *_srtncpy(char *dest, char *src, int n);
 #endif
