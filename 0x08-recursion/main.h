ea8af082701a7dfa3c2772bda80dcd23396bfd22
anir agram <anirpoke@gmail.com>
2022-09-28 06:16:37 -0700

    task0 commit

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,26 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void _puts_recursion(char *s);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_env(char *s);
+
+int _putchar(char c);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a,int n);
+
+char *_strcpy(char *dest, char *src);
+
+#endif
