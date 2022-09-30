a49364d04c31fca54630f22677e4816e6a453f1d
anir agram <anirpoke@gmail.com>
2022-09-30 11:15:21 -0700

    task0 commit

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_srtncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *s);
+char *cap_string(char *s);
+char *leet(char *s);
+void print_number(int n);
+
+
+
+#endif
