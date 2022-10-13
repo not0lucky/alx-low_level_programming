017ffa4315dad93aa87485811b0fcf8db94795a3
anir agram <anirpoke@gmail.com>
2022-10-13 13:24:09 -0700

    task0 commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,12 @@
 #include "main.h"
 /**
- * _isalpha - checks for a lowercase or uppercase character.
- *
- * @c: is c an integer argument
- *
- * Return: Always 0.
+ *_islower - checks for a lowercase character
+ *@c: letter being tested
+ * Return: Always 0
  */
-int _isalpha(int c)
+int _islower(int c)
 {
-	if (c >= 65 && c <= 90)
-		return (1);
-	else if (c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
